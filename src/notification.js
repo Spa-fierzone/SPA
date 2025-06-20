c24022d3df65b16a32017ffd0e3f5829d1c84219
Phitran1325 <phiphitran1325@gmail.com>
2025-06-20 07:37:00 +0700

    update feedback

---
+++ src/notification.js
@@ -0,0 +1,7 @@
+// Notification logic updated
+// src/notification.js
+function notifyUser(message) {
+    console.log("🔔 Notification:", message);
+  }
+  notifyUser("You have a new appointment.");
+  
